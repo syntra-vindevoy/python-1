e2e78b0e758304e4b4b712aae05214b646af3363
Jorick Vandoorne <jvdoorne@protonmail.com>
2022-09-22 21:24:40 +0200

    Add Chapter 1, Exercise 1

---
+++ think-python/ch01/ex01_1.py
@@ -0,0 +1,15 @@
+""" Think Python: Chapter 1, Exercise 1.1 (p.9)
+jvdoorne, @Syntra 22/09/2022
+"""
+
+print("1: SyntaxError: Missing parentheses in call to 'print'")
+
+print("2a: SyntaxError: EOL while scanning string literal")
+print("2b: NameError: name [string] is not defined")
+
+print("3a: Positive integer is read as a standard integer.")
+print("3b: Python interprets this as 2 + (+2).")
+
+print("4: Leading zero is dropped: 02 is interpreted as integer 2")
+
+print("5: SyntaxError: invalid syntax")
