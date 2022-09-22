801c5cc9e6e35d58d6331c936db199c721cc0aa1
Jorick Vandoorne <jvdoorne@protonmail.com>
2022-09-22 21:27:23 +0200

    Add Chapter 2, Exercise 1

---
+++ think-python/ch02/ex02_1.py
@@ -0,0 +1,14 @@
+""" Think Python Chapter 2, Exercise 2.1 (p.18)
+jvdoorne, @Syntra, 22/09/2022
+"""
+
+print("1. SyntaxError: can't assign to literal.")
+
+print("2. Both variables x and y are assigned the value 1.")
+
+print("3. The semicolon gets ignored by the interpreter.")
+
+print("4. SyntaxError: invalid syntax, (unless the last character is "
+      "an int, then it is interpreted as a float)")
+
+print("5. It gets interpreted as a variable named xy")
