c071502311096f2d50656a857e120faeec027cf0
christophebecker <ch.becker@hotmail.be>
2022-10-13 21:33:59 +0200

    §(

---
+++ 5_2_1.py
@@ -0,0 +1,23 @@
+
+import datetime
+import time
+
+# local_time = time.ctime()
+# local_time2 = datetime.datetime.utcnow()
+#
+# print(local_time2)
+
+time_today = datetime.datetime(day=13, month=10, year=2022)
+time_today2 = time.ctime()
+print(time_today)
+print(time_today2)
+
+
+
+
+
+
+#Thu Oct 13 19:34:46 2022
+
+#2022-10-13 17:35:07.524040
+
