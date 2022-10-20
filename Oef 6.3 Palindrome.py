33ff52660c8dd7e235f59e8c6c4ead118464c227
david <davidvanmeenen@hotmail.com>
2022-10-20 19:33:01 +0200

    Oefeningen H1 tot H5, tot verbetering vatbaar

---
+++ Oef 6.3 Palindrome.py
@@ -0,0 +1,21 @@
+a = "redivider"
+b = "not_a_palindrome"
+
+def first(word):
+    return word[0]
+
+def last(word):
+        return word[-1]
+
+def middle(word):
+        return word[1:-1]
+
+def is_palindrome(word):
+    for i in range((int(len(a)))/2):
+        word[i] == word[-i]
+
+
+#print(first(a))
+#print(last(a))
+#print(middle(a))
+print(is_palindrome(a))
