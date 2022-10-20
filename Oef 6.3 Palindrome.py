4eafa0ce9ac1c818fadcf6e90840852b9334de84
david <davidvanmeenen@hotmail.com>
2022-10-20 20:29:59 +0200

    Oefeningen H1 tot H5, tot verbetering vatbaar

--- Oef 6.3 Palindrome.py
+++ Oef 6.3 Palindrome.py
@@ -10,15 +10,39 @@ def last(word):
 def middle(word):
         return word[1:-1]
 
-def is_palindrome(word):
-    for i in range((int(len(a)))/2):
-        word[i] == word[-i]
+def length(word):
+
+    for i in range(int(len(word) / 2)):
+        return word[0:i-1]
+     #   i = i-1
+
+print(length(a))
+"""
+def test(word):
+    return (int(len(word) / 2))
 
 
+print(test(a))
 
 
+#def is_palindrome(word):
+#    for i in range((int(len(a)))/2):
+#         if word[i] == word[-i]:
 
+        #for i in range((int(len(a)))/2):
+         #   word[i] == word[-i]
+
+ #   else:
+#      print("not a palindrome")
+
+#def test():
+#    for i in range[0:((int(len(a)))/2):]:
+      #      word[i] == word[-i]
+
+#print(test(a))
 #print(first(a))
 #print(last(a))
 #print(middle(a))
-print(is_palindrome(a))
+#print(is_palindrome(a))
+
+"""
