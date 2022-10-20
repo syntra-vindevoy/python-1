c50472ad6f1b251827544cbaab040f5f3db5cdf0
Yves Vindevogel <yves.vindevogel@asynchrone.com>
2022-10-20 21:50:39 +0200

    Oefeningen tem hoofdstuk 6

---
+++ ex_6_4.py
@@ -0,0 +1,12 @@
+def is_power(a, b):
+    if a == b:
+        return True
+
+    if a % b != 0:
+        return False
+
+    return is_power(a / b, b)
+
+
+print(is_power(9, 3))
+print(is_power(9, 2))
