7cd87f1e22d6c69ac3467313747757de2cbe36fe
christophebecker <ch.becker@hotmail.be>
2022-11-17 19:17:04 +0100

    gitignore + oefeningen 6

---
+++ prime.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+
+def isprime(num):
+    for n in range(2,int(num**0.5)+1):
+        if num%n==0:
+            return False
+    return True
+
+
+list = []
+
+before = datetime.now()
+
+som = 0
+
+n = 1
+while True:
+    if isprime(n) == True:
+        som += 1
+        if som == 10000:
+            print(n)
+            break
+        n += 1
+    n+=1
+
+after = datetime.now()
+
+print(after - before)
+
