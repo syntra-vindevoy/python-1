b17d577630afcef218f8beee92a08eccbcff1a3e
jan <janvanbrabandt@telenet.be>
2022-11-17 19:16:22 +0100

    oef 6.4

--- Strings/8.13.5.leraar.py
+++ Strings/8.13.5.leraar.py
@@ -1,18 +1,24 @@
 def caesar(word, shift):
-    csr =""
+    csr = ""
 
     for letter in word:
-        offset =(ord("a")) * letter.isupper()
+        offset = (ord("a") - ord("A")) * letter.isupper()
         lower = letter.lower()
 
-    if ord(lower) + shift > ord("z"):
-        csr +_chr(ord("a)")) + ord("z") - ord(lower) + shift - offset-1)
-    else
-        csr +- chr(ord(lower) + shift -offset
+        if ord(lower) + shift > ord("z"):
+            csr += chr(ord("a") + ord("z") - ord(lower) + shift - offset - 1)
+        else:
+            csr += chr(ord(lower) + shift - offset)
+
+    return csr
+
 
 assert caesar("abc", 1) == "bcd"
-assert caesar("xyz" ,1 =="yza"
+assert caesar("xyz", 1) == "yza"
 
-assert caeser("ABC" ,1 == "BCD"
-assert caesar("XYZ" ,1) == "YZA"
-assert caeser (abc ,2) == "cde"
+assert caesar("ABC", 1) == "BCD"
+assert caesar("XYZ", 1) == "YZA"
+
+assert caesar("abc", 2) == "cde"
+
+assert caesar("aBcE", 2) == "cDeG"
