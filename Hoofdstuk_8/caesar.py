e042580136d33d6cf8592027dc5752eb5b7bd119
Yves Vindevogel <yves.vindevogel@asynchrone.com>
2022-11-17 19:18:44 +0100

    While loop

--- Hoofdstuk_8/caesar.py
+++ Hoofdstuk_8/caesar.py
@@ -4,9 +4,12 @@ plain_text = input("What string you want to encrypt?" )
 shift = int(input("Which key? "))
 shift %= 26
 
-alphabet = string.ascii_lowercase
-shifted = alphabet[shift:] + alphabet[:shift]
-table = str.maketrans(alphabet, shifted)
+alphabet_lower = string.ascii_lowercase
+shifted_lower = alphabet_lower[shift:] + alphabet_lower[:shift]
+
+alphabet_upper = string.ascii_uppercase
+shifted_upper = alphabet_upper[shift:] + alphabet_upper[:shift]
+table = str.maketrans(alphabet_lower + alphabet_upper, shifted_lower + shifted_upper)
 
 encrypted = plain_text.translate(table)
 
