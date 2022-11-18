25725625601c2c1d74499086bc3435b88828c76f
christophebecker <ch.becker@hotmail.be>
2022-11-18 16:01:51 +0100

    gitignore + oefeningen 6

--- 9_1.py
+++ 9_1.py
@@ -1,7 +1,21 @@
-with open("words.txt", "r") as input.file:
-    lines = input.file.readlines()
+# with open("words.txt", "r") as input.file:
+#     lines = input.file.readlines()
+#
+# for line in lines:
+#     words = line.strip()
+#     if len(words) > 19:
+#         print(words)
 
-for line in lines:
-    words = line.strip()
-    if len(words) > 19:
-        print(words)
+
+
+def characterCount():
+    fin = open("words.txt")
+    for line in fin:
+        words = line.strip()
+        if len(words) > 19:
+            print(words)
+
+
+
+characterCount()
+
