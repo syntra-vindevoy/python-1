3943e09f256bf88bf1a4d3dfda93a9646694558f
christophebecker <ch.becker@hotmail.be>
2022-11-19 08:36:04 +0100

    gitignore + oefeningen 6

--- calender.py
+++ calender.py
@@ -1,14 +1,7 @@
-# Program to display calendar of the given month and year
 
-# importing calendar module
 import calendar
 
-yy = 2014  # year
-mm = 11    # month
+yy = 2022  # year
+mm = 12    # month
 
-# To take month and year input from the user
-# yy = int(input("Enter year: "))
-# mm = int(input("Enter month: "))
-
-# display the calendar
 print(calendar.month(yy, mm))
