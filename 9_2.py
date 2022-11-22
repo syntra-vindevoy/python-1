705e87b766b6a843ed11c5a9cbda02f1d8825b32
christophebecker <ch.becker@hotmail.be>
2022-11-22 19:07:15 +0100

    gitignore + oefeningen 9

---
+++ 9_2.py
@@ -0,0 +1,16 @@
+fin = open('words.txt')
+def no_e(word):
+  if 'e' in word:
+    return True
+  else:
+    return False
+# count = 0
+# for word in fin:
+#     word = word.strip()
+#     if no_e(word):
+#         count = +1
+#         print(word)
+# percent = (count / 113809.0) * 100
+# print(str(percent))
+
+
--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-fruits = ["apple", "banana", "cherry"]
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":
+#     continue
+#   print(x)
 
-for x in fruits:
-  print(x)
 
+fruits2 = ("apple", "banana", "cherry")
+print(fruits2[0])
+print(fruits2[2])
