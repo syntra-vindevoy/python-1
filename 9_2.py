8037bf0b4f8f780f32d449fd1f59701af2073961
christophebecker <ch.becker@hotmail.be>
2022-11-22 19:09:04 +0100

    gitignore + oefeningen 9

--- 9_2.py
+++ 9_2.py
@@ -4,13 +4,6 @@ def no_e(word):
     return True
   else:
     return False
-# count = 0
-# for word in fin:
-#     word = word.strip()
-#     if no_e(word):
-#         count = +1
-#         print(word)
-# percent = (count / 113809.0) * 100
-# print(str(percent))
+
 
 
