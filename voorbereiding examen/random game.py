8ea7777dee6b4ab73ce8e6a7d5869be2f0c665d6
david <davidvanmeenen@hotmail.com>
2022-11-29 22:02:48 +0100

    voorbereidingen examen

--- voorbereiding examen/random game.py
+++ voorbereiding examen/random game.py
@@ -1,7 +1,10 @@
-from random import randrange
+from random import randint
 
 def random_game():
-    number = randrange.randint(1, 9)
+    number = randint(1, 9)
 
-    while input() != number and input():
+    while input() != number and input() != 0:
         continue
+    print("fuck you won")
+
+random_game()
