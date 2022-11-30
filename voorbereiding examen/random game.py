2e4db8c03532d1db70f16d7724ced33616659c05
david <davidvanmeenen@hotmail.com>
2022-11-30 22:28:14 +0100

    voorbereidingen examen

--- voorbereiding examen/random game.py
+++ voorbereiding examen/random game.py
@@ -4,10 +4,14 @@ from random import randint
 def random_game():
     number = randint(1, 9)
     x = 11
-    while number != x and x != 0:
+    while number != x :
         print(x)
         print(number)
-        x = input("enter the number: ")
+        x = int(input("enter the number: "))
+        if  x == 0:
+            print("you quit")
+            break
+
     print("you won")
 
 
