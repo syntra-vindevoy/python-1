6d396f2bee26f2744f069cec3783f5c460eabfec
christophebecker <ch.becker@hotmail.be>
2022-12-01 20:02:23 +0100

    oef 5 calculator

--- Bord oefeningen/oef_4.py
+++ Bord oefeningen/oef_4.py
@@ -1,20 +1,3 @@
-
-def add(x, y):
-    return x + y
-
-
-def subtract(x, y):
-    return x - y
-
-
-def multiply(x, y):
-    return x * y
-
-
-def divide(x, y):
-    return x / y
-
-
 print("Wat wil je doen?")
 print("1.+")
 print("2.-")
@@ -23,7 +6,6 @@ print("4./")
 print("5.Stop")
 
 while True:
-    # take input from the user
     keuze = input("Kies 1 2 3 4 5: ")
     if keuze == "5":
         break
@@ -33,13 +15,13 @@ while True:
         num2 = int(input("Tweede cijfer: "))
 
         if keuze == "1":
-            print(add(num1, num2))
+            print(num1 + num2)
 
         elif keuze == "2":
-            print(subtract(num1, num2))
+            print(num1 - num2)
 
         elif keuze == "3":
-            print(multiply(num1, num2))
+            print(num1 * num2)
 
         elif keuze == "4":
-            print(divide(num1, num2))
+            print(num1 / num2)
