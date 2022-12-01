a24e429c82f5942f2ee80e0cdb711e05c2498ff5
christophebecker <ch.becker@hotmail.be>
2022-12-01 20:20:59 +0100

    oef 5 calculator

--- Bord oefeningen/bord_oef_5.py
+++ Bord oefeningen/bord_oef_5.py
@@ -2,16 +2,3 @@ import random
 
 a = random.randint(1, 10000)
 print(a)
-#
-# while True:
-#     guess = int(input("Enter number: "))
-#     if guess == a:
-#         print("You Win! ")
-#         break
-#
-#     elif guess < a:
-#         print("Higher")
-#
-#     else:
-#         print("Lower")
-#         continue
