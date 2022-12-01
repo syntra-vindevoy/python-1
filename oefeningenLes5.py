7a9bef6c743d8916dd11222ba3c00bfa1237e02e
joris <joris.caushaj@gmail.com>
2022-12-01 21:59:39 +0100

    Gitignore

--- oefeningenLes5.py
+++ oefeningenLes5.py
@@ -1,14 +1,16 @@
 def find_number(n):
     counter = 1
     lowest = 0
-    highest = 10000
+    highest = 1000000000
     guess = int((highest-lowest)/2)
     while lowest != highest:
         if guess < n:
+            print(guess)
             lowest = int(guess)
             guess += int((highest-lowest)/2)
             counter += 1
         elif guess > n:
+            print(guess)
             highest = int(guess)
             guess -= int((highest-lowest)/2)
             counter += 1
