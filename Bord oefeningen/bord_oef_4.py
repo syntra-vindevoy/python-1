aa8be0f446924fa11701d77ca88f55401da56480
christophebecker <ch.becker@hotmail.be>
2022-12-01 20:14:45 +0100

    oef 5 calculator

---
+++ Bord oefeningen/bord_oef_4.py
@@ -0,0 +1,27 @@
+print("Wat wil je doen?")
+print("1.+")
+print("2.-")
+print("3.*")
+print("4./")
+print("5.Stop")
+
+while True:
+    keuze = input("Kies 1 2 3 4 5: ")
+    if keuze == "5":
+        break
+
+    if keuze in ("1", "2", "3", "4", "5"):
+        num1 = int(input("Eerste nummer: "))
+        num2 = int(input("Tweede nummer: "))
+
+        if keuze == "1":
+            print(num1 + num2)
+
+        elif keuze == "2":
+            print(num1 - num2)
+
+        elif keuze == "3":
+            print(num1 * num2)
+
+        elif keuze == "4":
+            print(num1 / num2)
