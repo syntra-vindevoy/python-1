5ced7e7f5b97efdc515c5ead5aa26c1c9dcfd4f3
david <davidvanmeenen@hotmail.com>
2022-12-06 18:40:27 +0100

    voorbereidingen examen

--- dow_Tomohiko_Sakamoto.py
+++ dow_Tomohiko_Sakamoto.py
@@ -9,12 +9,17 @@ def day_of_the_week(y, m, d):
     t = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4]
 
     # if month is less than 3 reduce year by 1
-    if (m < 3):
+    if m < 3:
         y = y - 1
 
-    return (y + y // 4 - y // 100 + y // 400 + t[m - 1] + d) % 7
+    x = (y + y // 4 - y // 100 + y // 400 + t[m - 1] + d) % 7
 
+    # sunday is 7 instead of 0
 
+    if x % 7 == 0:
+        return 7
+    else:
+        return x
 
 
 # Driver Code
@@ -22,17 +27,17 @@ def day_of_the_week(y, m, d):
 # month = 7
 # year = 2017
 
-#print(day_of_the_week(year, month, day))
+# print(day_of_the_week(year, month, day))
 
 if __name__ == "__main__":
     assert day_of_the_week(2022, 11, 17) == 4
     assert day_of_the_week(2022, 11, 16) == 3
     assert day_of_the_week(1983, 3, 23) == 3
-    assert day_of_the_week(2022,12, 4) == 0
+    assert day_of_the_week(2022, 12, 4) == 0
 
-    print (day_of_the_week(2022, 11, 17))
-    print (day_of_the_week(2022, 11, 16))
-    print (day_of_the_week(1983, 3, 23))
+    print(day_of_the_week(2022, 11, 17))
+    print(day_of_the_week(2022, 11, 16))
+    print(day_of_the_week(1983, 3, 23))
     print(day_of_the_week(2022, 1, 1))
-    print(day_of_the_week(2022,12, 4))
+    print(day_of_the_week(2022, 12, 4))
 # This code is contributed by Nikita Tiwari.
