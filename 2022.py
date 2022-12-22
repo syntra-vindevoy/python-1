71b90eea9c2a964ddde285eae02a1110dadf8316
jan <janvanbrabandt@telenet.be>
2022-12-22 18:36:21 +0100

    tataa

--- 2022.py
+++ 2022.py
@@ -1,5 +1,7 @@
+year = int(input("What year do you wish to print a calendar for? "))
 def isleapyear(year):
-    """Using the algorithm provided in the worksheet, this function determines whether any given year is a leap year or not"""
+    """Using the algorithm provided in the worksheet, this function determines whether any given year is a leap year
+    or not """
     if 0 == year % 400:
         return True
     elif 0 == year % 100:
@@ -15,9 +17,9 @@ def days_in_month(month, year2):
     *determining the length of any given month,
     *the year is given as a parameter as well because in a leap year, February is a day longer
     What this function does,
-    *is checking in which one of the two lists the month is in and returning 31 or 30, unless of course the given month is February in which case
-    *is checking whether the given year is a leap year or not and then returns 29 or 28 respectively.
-    """
+    *is checking in which one of the two lists the month is in and returning 31 or 30,
+    unless of course the given month is February in which case
+    *is checking whether the given year is a leap year or not and then returns 29 or 28 respectively. """
     list1 = [1, 3, 5, 7, 8, 10, 12]
     list2 = [4, 6, 9, 11]
     if month != 2:
@@ -52,7 +54,7 @@ def name_day(b):
     """returns the name of the day of the week, which is the parameter b"""
     # list=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    return list[b - 1]
+    return days[b - 1]
 
 
 def month_name(m):
@@ -67,45 +69,47 @@ def calendar(year):
     *for every day of every month, whether or not it is the first day of the month,
     *the last day of the week, the last day of the month,
     * or the last day of the week and the first day of the month
-    *or the last day of the month and the last day of the week as week, to then place it appropriately in the printed line"""
+    *or the last day of the month and the last day of the week as week,
+    to then place it appropriately in the printed line"""
     kalenderweek = 0
+    print(f"THIS IS A CALENDAR FOR {year}")
     for i in range(1, 13):
         string = "        " + month_name(i) + " " + str(year)
         # week_names= "W  " + "Zo " + "Ma  " + "Di  " + "Woe  " + "Do  " + "Fr  " + "Sa  "
         week_names = "W  " + "Ma  " + "Di  " + "Woe  " + "Do  " + "Fr  " + "Sa  " + "Zo "
         print(string + "\n" + week_names)
         teller = 0
-        firstDay = week_day(1, i, year)
+        firstday = week_day(1, i, year)
         n = 1
         for j in range(1, days_in_month(i, year) + 1):
             if teller == 0:
-                print('%02d' % (kalenderweek), end=" ")
-                if (j == 1):
-                    while n < firstDay:
+                print('%02d' % kalenderweek, end=" ")
+                if j == 1:
+                    while n < firstday:
                         n += 1
                         print("  ", end="  ")
                         teller += 1
-                    if firstDay == 7:
-                        print('%2d' % (j), end="  ")
+                    if firstday == 7:
+                        print('%2d' % j, end="  ")
                         print("\n")
                     else:
-                        print('%2d' % (j), end="  ")
+                        print('%2d' % j, end="  ")
                 else:
-                    print('%2d' % (j), end="  ")
+                    print('%2d' % j, end="  ")
                 teller += 1
             elif teller != 6:
-                print('%2d' % (j), end="  ")
+                print('%2d' % j, end="  ")
                 teller += 1
             if j == days_in_month(i, year) and week_day(j, i, year) != 7:
                 print("\n ")
             if week_day(j, i, year) == 7:
                 if j > 1 and j != days_in_month(i, year):
-                    print('%2d' % (j), end="  ")
+                    print('%2d' % j, end="  ")
                     kalenderweek += 1
                     teller = 0
                     print("\n")
                 elif j == days_in_month(i, year):
-                    print('%2d' % (j), end="  ")
+                    print('%2d' % j, end="  ")
                     kalenderweek += 1
                     print("\n")
                 elif j == 1:
@@ -113,4 +117,4 @@ def calendar(year):
                     teller = 0
 
 
-calendar(2024)
+calendar(year)
