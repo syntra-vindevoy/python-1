beaef27a026500ca370ca2ed68351f710efd04db
Jorick Vandoorne <jvdoorne@protonmail.com>
2022-12-27 21:52:41 +0100

    Add Chapter 10, Exercise 10

---
+++ think-python/ch10/ex10_10.py
@@ -0,0 +1,28 @@
+""" Think Python Chapter 10, Exercise 10.10 (p.101)
+jvdoorne, @Home, 27/12/2022
+"""
+
+
+def in_bisect(word: str, collection: list) -> bool:
+    low, high = 0, len(collection)
+    while low <= high:
+        pivot = (high + low) // 2
+        guess = collection[pivot]
+        if guess == word:
+            return True
+        elif guess < word:
+            low = pivot + 1
+        else:
+            high = pivot - 1
+    return False
+
+
+if __name__ == '__main__':
+
+    with open('../assets/words.txt') as f:
+        words = sorted([x.strip().lower() for x in f.readlines()])
+
+    assert in_bisect('aa', words)
+    assert in_bisect('bisect', words)
+    assert in_bisect('zymurgy', words)
+    assert not in_bisect('syntra', words)       # Not in words.txt
